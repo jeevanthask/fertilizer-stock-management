1352d223558e2545fb75d4ead4f69e14437b6a9b
jeevanthask <jeevanthanandika@gmail.com>
2021-08-10 11:12:35 +0530

    dummy db.json file updated

--- backend/routes/signupfarmers.router.js
+++ backend/routes/signupfarmers.router.js
@@ -4,20 +4,22 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const router = express.Router();
 
-let Book = require("../models/addbook.model");
+let Farmer = require("../models/farmers.model");
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-router.route("/addbook").post(function (req, res) {
-  let book = new Book(req.body);
-  book
+router.route("/addfarmer").post(function (req, res) {
+  let farmer = new Farmer(req.body);
+  farmer
     .save()
-    .then((book) => {
-      res.status(200).json({ book: "book added successfully" });
+    .then((farmer) => {
+      res.status(200).json({ farmer: "farmer added successfully" });
     })
     .catch((err) => {
-      res.status(400).send("adding book failed");
+      res.status(400).send("adding farmer failed");
     });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(" http://localhost:3000", {
+  useNewUrlParser: true,
 });
+const connection = mongoose.connection;
+
+connection.once("open", function () {
+  console.log("mongodb connection established successfully");
+});
+
+app.use("/Dashboard/addfarmer", require("./routes/signupfarmers.router"));
 
 app.listen(PORT, () => console.log(`app is listening in port ${PORT} ...`));
