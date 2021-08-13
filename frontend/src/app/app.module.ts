0e2af997f1df7b7f65a8593e7da5233f56e6ec70
jeevanthask <jeevanthanandika@gmail.com>
2021-08-13 16:29:46 +0530

    farmers signup component added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddfarmersComponent } from './components/addfarmers/addfarmers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddfarmersComponent
   ],
   imports: [
     BrowserModule
