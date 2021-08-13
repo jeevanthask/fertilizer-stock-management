e7425974797f530c7fe759c5b291cb60affc9e3e
jeevanthask <jeevanthanandika@gmail.com>
2021-08-13 08:23:24 +0530

    first reuest sent to mongodb successfully

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,15 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// mongoose.connect("mongodb://localhost:3000", {
-//   useNewUrlParser: true,
-// });
-
-mongoose.connect("mongodb://localhost:3000", {
+mongoose.connect("mongodb://127.0.0.1:27017/FertilizerDB", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
-
 const connection = mongoose.connection;
 
 connection.once("open", function () {
   console.log("mongodb connection established successfully");
 });
 
-app.use("/Dashboard/addfarmer", require("./routes/signupfarmers.router"));
+app.use("/Dashboard/admin", require("./routes/signupfarmers.router"));
 
 app.listen(PORT, () => console.log(`app is listening in port ${PORT} ...`));
