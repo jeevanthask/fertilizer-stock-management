f3b90a53edc6f7c0d2252d4f88867bcb4962a0ab
jeevanthask <jeevanthanandika@gmail.com>
2021-08-14 11:52:58 +0530

    new modules added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,14 +8,15 @@ import {AddfarmersComponent} from './components/addfarmers/addfarmers.component'
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {ReactiveFormsModule} from "@angular/forms";
-import { FarmerslistComponent } from './components/farmerslist/farmerslist.component';
-import { AddfertilizersComponent } from './components/addfertilizers/addfertilizers.component';
-import { FertilizerslistComponent } from './components/fertilizerslist/fertilizerslist.component';
-import { AddofficersComponent } from './components/addofficers/addofficers.component';
-import { OfficerslistComponent } from './components/officerslist/officerslist.component';
-import { AddsellingcompanyComponent } from './components/addsellingcompany/addsellingcompany.component';
-import { SellingcompanieslistComponent } from './components/sellingcompanieslist/sellingcompanieslist.component';
-
+import {FarmerslistComponent} from './components/farmerslist/farmerslist.component';
+import {AddfertilizersComponent} from './components/addfertilizers/addfertilizers.component';
+import {FertilizerslistComponent} from './components/fertilizerslist/fertilizerslist.component';
+import {AddofficersComponent} from './components/addofficers/addofficers.component';
+import {OfficerslistComponent} from './components/officerslist/officerslist.component';
+import {AddsellingcompanyComponent} from './components/addsellingcompany/addsellingcompany.component';
+import {SellingcompanieslistComponent} from './components/sellingcompanieslist/sellingcompanieslist.component';
+import {AccordionModule} from 'primeng/accordion';
+import {SidebarModule} from 'primeng/sidebar';
 
 @NgModule({
   declarations: [
@@ -30,12 +31,14 @@ import { SellingcompanieslistComponent } from './components/sellingcompanieslist
     AddsellingcompanyComponent,
     SellingcompanieslistComponent
   ],
-    imports: [
-        BrowserModule,
-        InputTextModule,
-        ButtonModule,
-        ReactiveFormsModule
-    ],
+  imports: [
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    AccordionModule,
+    SidebarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
