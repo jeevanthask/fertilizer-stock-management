2d43b863ee57a3e5dcbeac253cd55c38f1c28f6d
jeevanthask <jeevanthanandika@gmail.com>
2021-08-18 08:56:59 +0530

    company adding component finished

--- frontend/src/app/components/addsellingcompany/addsellingcompany.component.ts
+++ frontend/src/app/components/addsellingcompany/addsellingcompany.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CompanyService} from "../../services/company.service";
+
 
 @Component({
   selector: 'app-addsellingcompany',
@@ -7,7 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddsellingcompanyComponent implements OnInit {
 
-  constructor() { }
+  createForm: FormGroup;
+
+  constructor(private companyService: CompanyService, private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      firstname: ['', Validators.required],
+      secondname: '',
+      email: '',
+      telephone: '',
+      address: ''
+    });
+  }
+
+  addCompany(firstname: any, secondname: any, email: any, telephone: any, address: any) {
+
+    console.log(firstname)
+    console.log(secondname)
+    console.log(email)
+    console.log(telephone)
+    console.log(address)
+
+    this.companyService.addCompany(firstname, secondname, email, telephone, address).subscribe(() => {
+      alert("The company added successfully!!")
+    });
+
+    this.createForm = this.fb.group({
+      firstname: ['', Validators.required],
+      secondname: '',
+      email: '',
+      telephone: '',
+      address: ''
+    })
+
+  };
 
   ngOnInit(): void {
   }
--- frontend/src/app/services/company.service.ts
+++ frontend/src/app/services/company.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  uri = "http://localhost:4000"
+
+  constructor(private http: HttpClient) {
+  }
+
+  addCompany(firstname: any, secondname: any, email: any, telephone: any, address: any) {
+    const company = {
+      firstname: firstname,
+      secondname: secondname,
+      email: email,
+      telephone: telephone,
+      address: address
+    };
+    return this.http.post(`${this.uri}/Dashboard/admin/addcompany`, company)
+  }
 }
