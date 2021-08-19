b5e46837ad65b30796f5384d672b375969448f49
jeevanthask <jeevanthanandika@gmail.com>
2021-08-19 20:18:44 +0530

    officers list added

--- frontend/src/app/components/officerslist/officerslist.component.ts
+++ frontend/src/app/components/officerslist/officerslist.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { OfficerService } from 'src/app/services/officer.service';
 
 @Component({
   selector: 'app-officerslist',
   templateUrl: './officerslist.component.html',
-  styleUrls: ['./officerslist.component.css']
+  styleUrls: ['./officerslist.component.css'],
 })
 export class OfficerslistComponent implements OnInit {
+  officers: any = [];
 
-  constructor() { }
+  constructor(private officerService: OfficerService) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.fetchFarmers();
   }
 
+  fetchFarmers() {
+    this.officerService.getOfficers().subscribe((data) => {
+      this.officers = data;
+      console.log(this.officers);
+    });
+  }
 }
