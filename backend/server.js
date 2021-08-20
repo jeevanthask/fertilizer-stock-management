99bd63b24db0fc37af6b3727e1d0659c65996f28
jeevanthask <jeevanthanandika@gmail.com>
2021-08-20 09:28:40 +0530

    deletefarmer.router added

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ connection.once("open", function () {
 
 app.use("/Dashboard/admin", require("./routes/signupfarmers.router"));
 app.use("/Dashboard/admin", require("./routes/getfarmers.router"));
+app.use("/Dashboard/admin", require("./routes/deletefarmer.router"));
 
 app.use("/Dashboard/admin", require("./routes/addfertilizer.router"));
 app.use("/Dashboard/admin", require("./routes/getfertilizers.router"));
