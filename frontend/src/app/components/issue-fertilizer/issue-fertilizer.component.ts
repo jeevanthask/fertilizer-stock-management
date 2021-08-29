a0a26d4559ffec5b9d5f490f1f87a11f722b6adc
jeevanthask <jeevanthanandika@gmail.com>
2021-08-29 22:31:12 +0530

    farmer search functionality added

--- frontend/src/app/components/issue-fertilizer/issue-fertilizer.component.ts
+++ frontend/src/app/components/issue-fertilizer/issue-fertilizer.component.ts
@@ -8,18 +8,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./issue-fertilizer.component.css'],
 })
 export class IssueFertilizerComponent implements OnInit {
-  farmers: any = [];
   createForm: FormGroup;
-
-  text: string = '';
-
-  results: any = [];
-  filteredResults: any = [];
+  farmers: any = [];
+  firstname: any;
+  secondname: any;
+  address: any;
 
   constructor(private farmerService: FarmerService, private fb: FormBuilder) {
     this.createForm = this.fb.group({
-      nic: '',
+      inputnic: '',
     });
+  }
+
+  searchFarmer(inputnic: any) {
+    const farmerObj = this.farmers.find((k: any) => k.nic == inputnic);
+
+    if (farmerObj == undefined) {
+      alert('No Farmer for this ID');
+      this.firstname = '';
+      this.secondname = '';
+      this.address = '';
+    } else {
+      this.firstname = farmerObj.firstname;
+      this.secondname = farmerObj.secondname;
+      this.address = farmerObj.address;
+
+      console.log(this.firstname);
+      console.log(this.secondname);
+      console.log(this.address);
+    }
   }
 
   ngOnInit() {
@@ -31,49 +48,5 @@ export class IssueFertilizerComponent implements OnInit {
       this.farmers = data;
       console.log(this.farmers);
     });
-  }
-
-  submitHandle(nic: any) {
-    console.log(nic);
-  }
-
-  loadFarmers() {
-    this.farmerService.getFarmers().subscribe((data) => {
-      this.results = data;
-
-      this.filteredResults = this.results.map((a: any) => a.nic);
-
-      console.log(this.filteredResults);
-
-      console.log(this.filteredResults.length);
-    });
-  }
-
-  // search(event: any) {
-  //   this.farmerService.getFarmers().subscribe((data) => {
-  //     this.results = data;
-
-  //     this.filteredResults = this.results.map((a: any) => a.nic);
-
-  //     console.log(this.filteredResults);
-
-  //     console.log();
-  //   });
-  // }
-
-  filterCountry(event: any) {
-    this.loadFarmers();
-
-    let filtered: any[] = [];
-    let query = event.query;
-    for (let i = 0; i < this.filteredResults.length; i++) {
-      let country = this.filteredResults[i];
-      console.log(country);
-      if (country.toLowerCase().indexOf(query.toLowerCase()) == 0) {
-        filtered.push(country);
-      }
-    }
-
-    this.filteredResults = filtered;
   }
 }
