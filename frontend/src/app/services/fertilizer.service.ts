3300d5763ee1ac40d8731f584a46c043156f86c8
jeevanthask <jeevanthanandika@gmail.com>
2021-09-01 16:51:51 +0530

    ferilizer stock functions added to service

--- frontend/src/app/services/fertilizer.service.ts
+++ frontend/src/app/services/fertilizer.service.ts
@@ -46,4 +46,46 @@ export class FertilizerService {
       fertilizer
     );
   }
+
+  //fertilizer stock service functions
+
+  addFertilizerSock(name: any, producttype: any, quantity: any) {
+    const fertilizer = {
+      name: name,
+      producttype: producttype,
+      quantity: quantity,
+    };
+    return this.http.post(
+      `${this.uri}/Dashboard/admin/addfertilizerstock`,
+      fertilizer
+    );
+  }
+
+  getFertilizersStock() {
+    return this.http.get(`${this.uri}/Dashboard/admin/getfertilizerstocks`);
+  }
+
+  getFertilizerStockById(id: any) {
+    return this.http.get(
+      `${this.uri}/Dashboard/admin/getfertilizerstock/${id}`
+    );
+  }
+
+  deleteFertilizerStock(id: any) {
+    return this.http.delete(
+      `${this.uri}/Dashboard/admin/deletefertilizerstock/${id}`
+    );
+  }
+
+  updateFertilizerStock(id: any, name: any, producttype: any, quantity: any) {
+    const fertilizer = {
+      name: name,
+      producttype: producttype,
+      quantity: quantity,
+    };
+    return this.http.put(
+      `${this.uri}/Dashboard/admin/updatefertilizerstock/${id}`,
+      fertilizer
+    );
+  }
 }
