6f25afe39b6e4becc6bbeaa784d9d6571853a1f3
jeevanthask <jeevanthanandika@gmail.com>
2021-10-06 09:34:47 +0530

    company alert updated

--- frontend/src/app/components/addsellingcompany/addsellingcompany.component.ts
+++ frontend/src/app/components/addsellingcompany/addsellingcompany.component.ts
@@ -1,16 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {CompanyService} from "../../services/company.service";
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CompanyService } from '../../services/company.service';
 
 @Component({
   selector: 'app-addsellingcompany',
   templateUrl: './addsellingcompany.component.html',
-  styleUrls: ['./addsellingcompany.component.css']
+  styleUrls: ['./addsellingcompany.component.css'],
 })
 export class AddsellingcompanyComponent implements OnInit {
-
   createForm: FormGroup;
+  displayModal: boolean = false;
 
   constructor(private companyService: CompanyService, private fb: FormBuilder) {
     this.createForm = this.fb.group({
@@ -18,33 +17,37 @@ export class AddsellingcompanyComponent implements OnInit {
       secondname: '',
       email: '',
       telephone: '',
-      address: ''
+      address: '',
     });
   }
 
-  addCompany(firstname: any, secondname: any, email: any, telephone: any, address: any) {
+  addCompany(
+    firstname: any,
+    secondname: any,
+    email: any,
+    telephone: any,
+    address: any
+  ) {
+    console.log(firstname);
+    console.log(secondname);
+    console.log(email);
+    console.log(telephone);
+    console.log(address);
 
-    console.log(firstname)
-    console.log(secondname)
-    console.log(email)
-    console.log(telephone)
-    console.log(address)
-
-    this.companyService.addCompany(firstname, secondname, email, telephone, address).subscribe(() => {
-      alert("The company added successfully!!")
-    });
+    this.companyService
+      .addCompany(firstname, secondname, email, telephone, address)
+      .subscribe(() => {
+        this.displayModal = true;
+      });
 
     this.createForm = this.fb.group({
       firstname: ['', Validators.required],
       secondname: '',
       email: '',
       telephone: '',
-      address: ''
-    })
-
-  };
-
-  ngOnInit(): void {
+      address: '',
+    });
   }
 
+  ngOnInit(): void {}
 }
