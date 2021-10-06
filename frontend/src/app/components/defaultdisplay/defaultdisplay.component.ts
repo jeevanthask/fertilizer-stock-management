8887c79f054e87899483733b99390bb6240c55ac
jeevanthask <jeevanthanandika@gmail.com>
2021-10-06 15:27:58 +0530

    card notations updated

--- frontend/src/app/components/defaultdisplay/defaultdisplay.component.ts
+++ frontend/src/app/components/defaultdisplay/defaultdisplay.component.ts
@@ -1,3 +1,6 @@
+import { OfficerService } from './../../services/officer.service';
+import { FarmerService } from './../../services/farmer.service';
+import { FertilizerService } from './../../services/fertilizer.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./defaultdisplay.component.css'],
 })
 export class DefaultdisplayComponent implements OnInit {
-  constructor() {}
+  fertilizers: any = [];
+  farmers: any = [];
+  officers: any = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private farmerService: FarmerService,
+    private ferilizerService: FertilizerService,
+    private officerService: OfficerService
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchFertilizers();
+    this.fetchFarmers();
+    this.fetchOfficers();
+  }
+
+  fetchFertilizers() {
+    this.ferilizerService.getFertilizers().subscribe((data) => {
+      this.fertilizers = data;
+      console.log(this.fertilizers);
+    });
+  }
+
+  fetchFarmers() {
+    this.farmerService.getFarmers().subscribe((data) => {
+      this.farmers = data;
+      console.log(this.farmers.length);
+    });
+  }
+
+  fetchOfficers() {
+    this.officerService.getOfficers().subscribe((data) => {
+      this.officers = data;
+      console.log(this.officers);
+    });
+  }
 }
